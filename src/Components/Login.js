7c9b08978d5a83d30e4e2a903fd1413f9528ffc7
Abhilash <abhilasht0007.t@gmail.com>
2024-03-22 10:12:34 +0530

    Sign In and Sign Up Form.

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import { bg_url } from "../Utils/Constants";
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const toggleForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
   return (
     <div>
       <Header />
@@ -9,19 +13,29 @@ const Login = () => {
         <img className="" alt="bg-img" src={bg_url} />
       </div>
       <form className="bg-black bg-opacity-85 rounded-md absolute mt-32 mx-auto right-0 left-0 px-16 py-10 flex flex-col w-[28rem] h-3/4">
-        <h1 className="text-white font-bold text-3xl my-4">Sign In</h1>
+        <h1 className="text-white font-bold text-3xl my-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            className="my-3 bg-blue-300 bg-opacity-30 text-white font-semibold border justify-center rounded-md py-3 px-2"
+            type="text"
+            placeholder="Enter Name"
+          />
+        )}
         <input
           className="my-3 bg-blue-300 bg-opacity-30 text-white font-semibold border justify-center rounded-md py-3 px-2"
           type="email"
           placeholder="Enter Email address"
         />
+
         <input
           className="my-3 justify-center bg-blue-300 bg-opacity-30 border text-white font-semibold rounded-md py-3 px-2"
           type="password"
           placeholder="Enter Password"
         />
         <button className="px-3 py-2 mt-6 text-white font-semibold rounded-md bg-red-700">
-          Sign In
+          {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
         <div className="flex justify-center mt-4">
           <a className="text-white" href="/">
@@ -29,16 +43,21 @@ const Login = () => {
           </a>
         </div>
         <div className="mt-4 flex">
-          <h1 className="text-gray-400 font-semibold">New to Netflix?</h1>
-          <a className="text-white font-semibold ml-2" href="/">
-            Sign up now.
-          </a>
+          {/* <h1 className="text-gray-400 font-semibold">New to Netflix?</h1> */}
+          <p
+            className="text-white font-semibold ml-2 cursor-pointer"
+            onClick={toggleForm}
+          >
+            {isSignInForm
+              ? "New to Netflix? Sign Up now"
+              : "Already registered? Sign In now"}
+          </p>
         </div>
         <div className="w-3/4">
-          <h1 className="text-gray-400 text-xs mt-4">
+          <p className="text-gray-400 text-xs mt-4 ml-2">
             This page is protected by Google reCAPTCHA to ensure you're not a
             bot.
-          </h1>
+          </p>
         </div>
       </form>
     </div>
